76b349002c5a6f2633e60dfebfecf3f665b42ea8
Sakhile Mahlalengangeni Mkhonta <135160725+SakhileEngineer95@users.noreply.github.com>
2026-06-14 13:42:11 +0200

    added env variables

--- src/database/supabaseClient.js
+++ src/database/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://soepadocivmmyufnwrot.supabase.co';        // ← Replace
-const supabaseAnonKey = 'sb_publishable_PQx4X9l1YaqU9kR_Eb770w_Wfe6NZ3W';             // ← Replace
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
